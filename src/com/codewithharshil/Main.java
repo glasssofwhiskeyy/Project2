fc9b503670cbc6a56f31eadc4efd04af2fc38897
glasssofwhiskeyy <harshilrattan4@gmail.com>
2023-03-23 13:47:55 -0700

    main method only

--- src/com/codewithharshil/Main.java
+++ src/com/codewithharshil/Main.java
@@ -3,8 +3,7 @@ package com.codewithharshil;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
+
 
     }
 }
